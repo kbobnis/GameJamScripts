33e326cb6822ff6abdac7240da44389fc6bfeac9
Krzysztof Bobnis <kbobnis@gmail.com>
2020-10-03 15:15:02 +0200

    Commiting KBobnis Open Source for now, but will change it to a package later. Maybe.

---
+++ Editor/File Operations/KbobnisFile.cs
@@ -0,0 +1,28 @@
+﻿
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+	public class KbobnisFile {
+		public static void Write(string text, string path = "Assets/Resources/test.txt") {
+
+			//Write some text to the test.txt file
+			StreamWriter writer = new StreamWriter(path, true);
+			writer.WriteLine(text);
+			writer.Close();
+
+			//Re-import the file to update the reference in the editor
+			AssetDatabase.ImportAsset(path);
+			TextAsset asset = Resources.Load(path) as TextAsset;
+		}
+
+		public static void ReadString() {
+			string path = "Assets/Resources/test.txt";
+
+			//Read the text from directly from the test.txt file
+			StreamReader reader = new StreamReader(path);
+			Debug.Log(reader.ReadToEnd());
+			reader.Close();
+		}
+
+}
--- Extensions/GameObjectExtensions.cs
+++ Extensions/GameObjectExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public static class GameObjectExtensions {
 
@@ -13,6 +14,36 @@ public static class GameObjectExtensions {
 			bounds.Encapsulate(col.bounds);
 		}
 		return bounds;
+	}
+
+	public static GameObject AddChildFromPrefab(this GameObject container, GameObject prefab, bool stretchAnchors = false) {
+		GameObject go = GameObject.Instantiate(prefab) as GameObject;
+		go.transform.SetParent(container.transform);
+		go.transform.localScale = new Vector3(1, 1, 1);
+		if (stretchAnchors) {
+			RectTransform rt = go.GetComponent<RectTransform>();
+			rt.SetSize(container.GetComponent<RectTransform>().GetSize());
+		}
+		go.SetActive(true);
+		return go;
+	}
+
+	public static T GetElementByName<T>(this GameObject go, string name) where T : Component {
+		foreach (Transform t in go.GetComponentsInChildren<Transform>(true)) {
+			if (t.gameObject.name == name) {
+				return t.GetComponent<T>();
+			}
+		}
+		throw new Exception("Child " + name + " not found in " + go.name);
+	}
+
+	public static GameObject GetElementByName(this GameObject go, string name) {
+		foreach (Transform t in go.GetComponentsInChildren<Transform>(true)) {
+			if (t.gameObject.name == name) {
+				return t.gameObject;
+			}
+		}
+		throw new Exception("Child " + name + " not found in " + go.name);
 	}
 
 }
