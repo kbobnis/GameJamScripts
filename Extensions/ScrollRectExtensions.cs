826b6dde253227834e734a4f1a189e71200390b4
Krzysztof Bobnis <kbobnis@gmail.com>
2020-11-24 22:59:36 +0100

    Added scroll rect extensions.

---
+++ Extensions/ScrollRectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public static class ScrollRectExtensions
+{
+	public static void ScrollToTop(this ScrollRect scrollRect)
+	{
+		////ScrollRect.verticalNormalizedPosition = 1f; //y axis is upside down, 1 is where the top lies.
+		scrollRect.normalizedPosition = new Vector2(0, 1);
+	}
+	public static void ScrollToBottom(this ScrollRect scrollRect)
+	{
+		scrollRect.normalizedPosition = new Vector2(0, 0);
+	}
+}
